2d7874726a74fc2811af28ac04df9e129f2965ef
韩毅 <hy01780305@alibaba-inc.com>
2024-08-21 19:54:45 +0800

    feat: 新增系统设置表

---
+++ seeders/20240821114741-setting.js
@@ -0,0 +1,18 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.bulkInsert('Settings', [{
+      name: '炼金实验室',
+      icp: '京ICP备12345678号',
+      copyright: '© 2024 Alchemy Lab Inc. All Rights Reserved.',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }], {});
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.bulkDelete('Settings', null, {});
+  }
+};
